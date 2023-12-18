1de622338e8bb631a6bd75c6bfe8992401dbc75e
Vraj Patel <vraj.patel.12390@gmail.com>
2023-12-18 00:53:11 -0500

    Fully responsive website is completed

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -4,7 +4,7 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import Button from "../Buttons/buttons";
 const Newsletter = () => (
   <div
-    className="w-[80%] lg:w-[70%] py-8 px-16 border-solid border-golden border-[1px] mt-12
+    className="w-[90%] lg:w-[70%] py-4 px-4 lg:px-12 flex flex-col justify-center items-center border-solid border-golden border-[1px] mt-12
    bg-black">
     <div className="text-center flex flex-col justify-center items-center">
       <p className="p_cormorant">NewsLetter</p>
@@ -19,9 +19,9 @@ const Newsletter = () => (
       </h1>
       <p className="p__opensans text-white">And never miss latest Updates!</p>
     </div>
-    <div className="flex flex-col lg:flex-row justify-center items-center mt-12">
+    <div className="flex flex-col justify-center items-center lg:flex-row  mt-4 lg:mt-12">
       <input
-        className="w-full mb-8 lg:mb-0 md:w-[400px] lg:w-[620px] border-2 border-solid border-golden rounded-md font-base text-white text-base mr-8 py-3 px-4 bg-black "
+        className="w-full mb-8 mr-0 lg:mr-8 lg:mb-0 md:w-[400px] lg:w-[620px] border-2 border-solid border-golden rounded-md font-base text-white text-base  py-3 px-4 bg-black "
         type="email"
         placeholder="Enter your email address"
       />
